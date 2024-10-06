e456f5b7e2a0b73942c96008b33b648ceee3f5fa
rhu1 <r.hu@qmul.ac.uk>
2024-10-06 15:38:35 +0100

    Continue

---
+++ src/test/scala/tmp/EATmp/Proto1/Proto1_B.scala
@@ -0,0 +1,48 @@
+package tmp.EATmp.Proto1
+
+import ea.runtime.{Actor, Done, Session}
+
+trait ActorB extends Actor {
+
+	def registerB[D <: Session.Data](port: Int, apHost: String, apPort: Int, d: D, f: (D, B1Suspend) => Done.type): Unit = {
+		val g = (sid: Session.Sid) => B1Suspend(sid, this)
+		enqueueRegisterForPeers(apHost, apPort, "Proto1", "B", port, d, f, g, Set("A"))
+	}
+}
+
+case class B1Suspend(sid: Session.Sid, actor: Actor) extends Session.SuspendState[Actor] {
+
+	def suspend[D <: Session.Data](d: D, f: (D, B1) => Done.type): Done.type = {
+		checkNotUsed()
+		val g = (op: String, pay: Object) => {
+			var succ: Option[Session.ActorState[Actor]] = None
+			val msg: B1 =
+			if (op == "L1") {
+				val s = EndB(sid, actor)
+				succ = Some(s)
+				L1B(sid, pay.asInstanceOf[String], s)
+			} else {
+				throw new RuntimeException(s"[ERROR] Unexpected op: ${op}(${pay})")
+			}
+			val done = f.apply(d, msg)
+			succ.get.checkUsed()
+			done
+		}
+		actor.setHandler(sid, "B", "A", g)
+		Done
+	}
+}
+
+sealed trait B1 extends Session.IState
+
+case class L1B(sid: Session.Sid, x: String, s: EndB) extends B1
+
+case class EndB(sid: Session.Sid, actor: Actor) extends Session.End[Actor] {
+
+	override def finish(): Done.type = {
+		checkNotUsed()
+		val done = super.finish()
+		actor.end(sid, "B")
+		done
+	}
+}
---
+++ src/test/scala/tmp/EATmp/TestTmp.scala
@@ -0,0 +1,74 @@
+package tmp.EATmp
+
+import ea.runtime.{Actor, Done, Session}
+import tmp.EATmp.Proto1.{A1, ActorA}
+
+object TestGen01 {
+
+    def main(args: Array[String]): Unit = {
+        println("Hello")
+
+        /*val foo = new AP("Foo", Set("A", "B"))
+        foo.spawn(8888)
+        Thread.sleep(1000)
+        FooA.spawn(); FooB.spawn()*/
+
+        //val proto1 = new AP("Proto1", Set("A", "B"))
+        val p1 = new Proto1.Proto1
+        p1.spawn(8888)
+        Thread.sleep(1000)
+
+        A.debug = true
+        B.debug = true
+
+        A.spawn(); B.spawn()
+    }
+}
+
+
+case class DataA() extends Session.Data
+case class DataB() extends Session.Data
+
+
+/* ... */
+
+object A extends Actor("MyA") with ActorA {
+
+    def spawn(): Unit = {
+        spawn(7777)
+        registerA(7777, "localhost", 8888, DataA(), a1)  // !!! mutable data
+    }
+
+    def a1(d: DataA, s: Proto1.A1): Done.type = {
+        //Done  // testing linearity
+        //s.sendL1(s"abc")  // testing linearity
+        finishAndClose(s.sendL1(s"abc"))
+    }
+}
+
+
+/* ... */
+
+object B extends Actor("MyB") with Proto1.ActorB {
+
+    def spawn(): Unit = {
+        spawn(6666)
+        registerB(6666, "localhost", 8888, DataB(), b1Init)  // !!! mutable data
+    }
+
+    def b1Init(d: DataB, s: Proto1.B1Suspend): Done.type = {
+        //Done  // testing linearity
+        s.suspend(d, b1)
+        //s.suspend(b1)  // testing linearity
+    }
+
+    def b1(d: DataB, s: Proto1.B1): Done.type = {
+        //Done  // testing linearity
+        s match {
+            case Proto1.L1B(sid, x, s) =>
+                //Done  // testing linearity
+                finishAndClose(s)
+        }
+    }
+}
+
